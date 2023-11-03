4748a9f75f97ec53d9904dee0358e603edd6805b
k266n <144876636+k266n@users.noreply.github.com>
2023-11-02 20:59:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width:350, 
+    height:300,
+    img_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML="<img id='capture_image'src="+data_uri+">";
+    });
+}
+console.log("ml5 version:",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/jtaIhmtJO/model.json",modelloaded);
+function modelloaded(){
+    console.log("model is loaded");
+    
+}
+function check(){
+    img=document.getElementById("capture_image");
+    classifier.classify(img,gotresults);
+
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;    
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
